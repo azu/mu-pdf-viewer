ae2976574f63867ca482127c5b41518106b7a907
azu <azuciao@gmail.com>
2016-08-21 21:23:59 +0900

    feat(electron): auto updater

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const electron = require("electron");
 const app = electron.app;
 const path = require("path");
 const windowStateKeeper = require('electron-window-state');
+const GhReleases = require('electron-gh-releases');
+const options = {
+    repo: 'azu/mu-pdf-viewer',
+    currentVersion: app.getVersion()
+};
+const updater = new GhReleases(options);
+// Check for updates
+// `status` returns true if there is a new update available
+updater.check((err, status) => {
+    if (!err && status) {
+        // Download the update
+        updater.download()
+    }
+});
+// When an update has been downloaded
+updater.on('update-downloaded', (info) => {
+    // Restart the app and install the update
+    updater.install()
+});
 const BrowserWindow = electron.BrowserWindow;
 const argv = require('minimist')(process.argv.slice(2));
 const qs = require("querystring");
